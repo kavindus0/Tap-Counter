219d0e7eb204642c151bd230c17ea57631acf23e
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-09 23:58:39 +0530

    Recreated App layout

--- lib/basecode.dart
+++ lib/basecode.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:info_popup/info_popup.dart';
+import 'package:animated_flip_counter/animated_flip_counter.dart';
 
 class BaseFile extends StatefulWidget {
   const BaseFile({super.key});
@@ -22,9 +24,19 @@ class _BaseFileState extends State<BaseFile> {
               },
               icon: const Icon(Icons.restart_alt)),
           const Spacer(),
-          const Text("Tap Counter"),
+          const Text(
+            "Tap Counter",
+            style: TextStyle(fontSize: 23),
+          ),
           const Spacer(),
-          const Icon(Icons.info),
+          const InfoPopupWidget(
+            contentTitle:
+                'Tap Below Body To Start Counting. If you tap on the RESET, the count will reset.',
+            child: Icon(
+              Icons.info,
+              color: Colors.green,
+            ),
+          ),
         ]),
         body: GestureDetector(
           onTap: () {
@@ -34,17 +46,31 @@ class _BaseFileState extends State<BaseFile> {
           },
           child: Center(
             child: Container(
-              color: Colors.red,
+              color: Colors.grey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   const Spacer(),
                   Center(
-                      child: Text(
-                    taps.toString(),
-                    style: const TextStyle(color: Colors.white, fontSize: 100),
-                  )),
+                    child: AnimatedFlipCounter(
+                      value: taps,
+                      duration: const Duration(milliseconds: 200),
+                      textStyle: TextStyle(color: Colors.white, fontSize: 150),
+                    ),
+                  ),
                   const Spacer(),
+                  FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      'Reset'.toUpperCase(),
+                      style: const TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        taps = 0;
+                      });
+                    },
+                  ),
                 ],
               ),
             ),
