586131c95a154e60eda42f74541916e7b27e6606
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-03-12 00:41:40 +0530

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'basecode.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
